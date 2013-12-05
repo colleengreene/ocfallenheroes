de3c281d9fac10c17d52909350c0385d8ddf64e1
Colleen Greene <colleen.e.greene@gmail.com>
2013-12-04 17:28:34 -0800

    Renamed headings in sidebar widgets.

--- customized-themes/seasons/items/show.php
+++ customized-themes/seasons/items/show.php
@@ -38,7 +38,7 @@ queue_css_file('lightbox');
     <!-- The following returns all of the files associated with an item. -->
     <?php if ((get_theme_option('Item FileGallery') == 1) && metadata('item', 'has files')): ?>
     <div id="itemfiles" class="element">
-        <h2><?php echo __('Associated Photos'); ?></h2>
+        <h2><?php echo __('Photos'); ?></h2>
         <div class="element-text">
 		<!--displays larger image sizes than thumbnails (Added by Colleen)--> 
 		<?php
@@ -51,14 +51,13 @@ queue_css_file('lightbox');
 		echo file_markup($itemFiles);
 		?>
 	  </div>
-        <p>Click on a thumbnail for full size image.</p>
     </div>
     <?php endif; ?>
 
     <!-- If the item belongs to a collection, the following creates a link to that collection. -->
     <?php if (metadata('item', 'Collection Name')): ?>
     <div id="collection" class="element">
-        <h2><?php echo __('Associated Collection'); ?></h2>
+        <h2><?php echo __('Collection'); ?></h2>
         <div class="element-text"><p><?php echo link_to_collection_for_item(); ?></p></div>
     </div>
     <?php endif; ?>
@@ -66,14 +65,14 @@ queue_css_file('lightbox');
     <!-- The following prints a list of all tags associated with the item -->
     <?php if (metadata('item', 'has tags')): ?>
     <div id="item-tags" class="element">
-        <h2><?php echo __('Associated Tags'); ?></h2>
+        <h2><?php echo __('Tags'); ?></h2>
         <div class="element-text"><?php echo tag_string('item'); ?></div>
     </div>
     <?php endif;?>
 
 <!-- Begin Added by Colleen -->
 <div>
-<h2>Record Timestamp</h2>
+<h2>Timestamp</h2>
 <strong>Date Added:</strong> <?php echo date('m/d/Y', strtotime($item->added)); ?><br />
 <strong>Last Updated:</strong> <?php echo date('m/d/Y', strtotime($item->modified)); ?></p>
 </div>
@@ -81,7 +80,7 @@ queue_css_file('lightbox');
 
     <!-- The following prints a citation for this item. -->
     <div id="item-citation" class="element">
-        <h2><?php echo __('Recommended Citation'); ?></h2>
+        <h2><?php echo __('Citation'); ?></h2>
         <div class="element-text"><?php echo metadata('item', 'citation', array('no_escape' => true)); ?></div>
     </div>
 
