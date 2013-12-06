a12cab91dcf23533d5e0eb2f1706a21c7a67ec3c
Colleen Greene <colleen.e.greene@gmail.com>
2013-12-05 17:04:38 -0800

    Added Disqus comments to Show Item view

--- customized-themes/seasons/items/show.php
+++ customized-themes/seasons/items/show.php
@@ -17,6 +17,27 @@ queue_css_file('lightbox');
     
     <?php fire_plugin_hook('public_items_show', array('view' => $this, 'item' => $item)); ?>
 
+<!-- -----Added by Colleen----- -->
+<div>
+<h2>Share a Comment, Question, Fact, or Memory</h2>
+<!-- Start Disqus -->
+<div id="disqus_thread">
+<h2>Share a Comment, Question, or Memory</h2>
+<script type="text/javascript">
+    /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+    var disqus_shortname = 'ocfallenheroes'; // required: replace example with your forum shortname
+
+    /* * * DON'T EDIT BELOW THIS LINE * * */
+    (function() {
+        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+        dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+    })();
+</script>
+<noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+<a href="http://disqus.com" class="dsq-brlink">blog comments powered by <span class="logo-disqus">Disqus</span></a>
+</div>
+</div>
 
 </div><!-- end primary -->
 
