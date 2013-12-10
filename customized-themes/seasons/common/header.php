953c90fd441f0b6e0ca95a63c4d68ca95afe7689
Colleen Greene <colleen.e.greene@gmail.com>
2013-12-10 09:30:51 -0800

    Updated with correct Google Analytics code
    Had the wrong code. No Analytics have been gathered so far.

--- customized-themes/seasons/common/header.php
+++ customized-themes/seasons/common/header.php
@@ -43,10 +43,10 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-3644638-18', 'ocfallenheroes.org');
+  ga('create', 'UA-25950858-2', 'ocfallenheroes.org');
   ga('send', 'pageview');
-</script>
 
+</script>
 <!-- ShareThis Tracking -->
 <script type="text/javascript">var switchTo5x=false;</script>
 <script type="text/javascript" src="http://w.sharethis.com/button/buttons.js"></script>
